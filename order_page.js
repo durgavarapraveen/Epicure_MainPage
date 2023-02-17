81eecccc479fdd2778471a9af68a9a7e50d803d0
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-02-17 16:44:25 +0530

    First Commit

---
+++ order_page.js
@@ -0,0 +1,13 @@
+
+// var i=0;
+// function returntxt() {
+//     let demoInput = document.getElementById("demoInput").value;
+//     i=i+demoInput;
+// }
+function increment() {
+    document.getElementById('demoInput').stepUp();
+}
+function decrement() {
+    document.getElementById('demoInput').stepDown();
+}
+
