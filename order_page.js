78548028ec8a85d69849ce9a4703fe5283bc9438
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-02-20 19:09:58 +0530

    Nav Bar

--- order_page.js
+++ order_page.js
@@ -1,13 +1,4 @@
-
-// var i=0;
-// function returntxt() {
-//     let demoInput = document.getElementById("demoInput").value;
-//     i=i+demoInput;
-// }
-function increment() {
-    document.getElementById('demoInput').stepUp();
-}
-function decrement() {
-    document.getElementById('demoInput').stepDown();
+function paymentpage() {
+    
 }
 
