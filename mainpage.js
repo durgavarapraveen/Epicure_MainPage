88c87430884d593797f9114dc47221d6a34b243e
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-02-20 11:56:36 +0530

    Responsive

--- mainpage.js
+++ mainpage.js
@@ -13,4 +13,13 @@ function carousel() {
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 
