6a6580d8328b87b27fac5a5d15c94aa53c262722
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-02-21 13:07:58 +0530

    popup page

--- order_page.js
+++ order_page.js
@@ -1,4 +1,14 @@
-function paymentpage() {
-    
+const arr = [74,26,100]; 
+var i=0;
+function save() {
+    for( i=0;i<3;i++) {
+        var amount  = document.getElementById('i').value * arr[i];
+    }
+    document.getElementById('amount').innerHTML = amount;
+
 }
 
+function myfunction() {
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle("show");
+}
